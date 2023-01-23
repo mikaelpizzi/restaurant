c2f30be7b10acad0f3ae68ca19f1d380a0e2b7fe
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2023-01-23 19:18:04 -0400

    feat: Implement laurels component

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React from "react";
+import { SubHeading } from "../../components";
+import { images, data } from "../../constants";
 
-import './Laurels.css';
+import "./Laurels.css";
+
+const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
+  <div className="app__laurels_awards-card">
+    <img src={imgUrl} alt="award" />
+    <div className="app__laurels_award-card_content">
+      <p className="p__cormorant" style={{ color: "#ddca87" }}>
+        {title}
+      </p>
+      <p className="p__cormorant">{subtitle}</p>
+    </div>
+  </div>
+);
 
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className="app__bg app__wrapper section__padding" id="awards">
+    <div className="app__wrapper_info">
+      <SubHeading title="Awards & Recognition" />
+      <h1 className="headtext_cormorant">Our Laurels</h1>
+      <div className="app__laurels_awards">
+        {data.awards.map((award) => (
+          <AwardCard key={award.title} award={award} />
+        ))}
+      </div>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.laurels} alt="laurels" />
+    </div>
   </div>
 );
 
