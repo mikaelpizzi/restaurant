99dc3564433bee6b4b7d8d4e0c7a604fd07f98f5
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2023-01-22 20:30:30 -0400

    feat: Implement state for overlay menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,53 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdOutlineRestaurantMenu } from "react-icons/md";
 import images from "../../constants/images";
 import "./Navbar.css";
 
-const Navbar = () => (
-  <nav className="app__navbar">
-    <div className="app_navbar-logo">
-      <img src={images.gericht} alt="logo" />
-    </div>
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
 
-    <ul className="app__navbar-links">
-      <li className="p__opensans">
-        <a href="#home">Home</a>
-      </li>
-      <li className="p__opensans">
-        <a href="#about">About</a>
-      </li>
-      <li className="p__opensans">
-        <a href="#menu">Menu</a>
-      </li>
-      <li className="p__opensans">
-        <a href="#awards">Awards</a>
-      </li>
-      <li className="p__opensans">
-        <a href="#contact">Contact</a>
-      </li>
-    </ul>
-
-    <div className="app__navbar-login">
-      <a href="#login" className="p__opensans">
-        Log In / Register
-      </a>
-      <div />
-      <a href="/" className="p__opensans">
-        Book Table
-      </a>
-    </div>
-
-    <div className="app__navbar-smallscreen">
-      <GiHamburgerMenu color="#fff" fontsize={27} onClick={() => {}} />
-      <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
-        <MdOutlineRestaurantMenu
-          fontSize={27}
-          className="overlay__close"
-          onClick={() => {}}
-        />
+  return (
+    <nav className="app__navbar">
+      <div className="app_navbar-logo">
+        <img src={images.gericht} alt="logo" />
       </div>
-      <ul className="app__navbar-smallscreen-links">
+
+      <ul className="app__navbar-links">
         <li className="p__opensans">
           <a href="#home">Home</a>
         </li>
@@ -64,8 +30,53 @@ const Navbar = () => (
           <a href="#contact">Contact</a>
         </li>
       </ul>
-    </div>
-  </nav>
-);
+
+      <div className="app__navbar-login">
+        <a href="#login" className="p__opensans">
+          Log In / Register
+        </a>
+        <div />
+        <a href="/" className="p__opensans">
+          Book Table
+        </a>
+      </div>
+
+      <div className="app__navbar-smallscreen">
+        <GiHamburgerMenu
+          color="#fff"
+          fontsize={27}
+          onClick={() => setToggleMenu(true)}
+        />
+
+        {toggleMenu && (
+          <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+            <MdOutlineRestaurantMenu
+              fontSize={27}
+              className="overlay__close"
+              onClick={() => setToggleMenu(false)}
+            />
+            <ul className="app__navbar-smallscreen-links">
+              <li className="p__opensans">
+                <a href="#home">Home</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#about">About</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#menu">Menu</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#awards">Awards</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
