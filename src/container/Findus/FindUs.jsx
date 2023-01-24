21385aa2c9703b4008f11a9fac17ae32bfb02987
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2023-01-24 00:27:49 -0400

    feat: Implement find us component

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React from "react";
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
 
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className="app__bg app__wrapper section__padding" id="contact">
+    <div className="app__wrapper_info">
+      <SubHeading title="Contact" />
+      <h1 className="headtext__cormorant" style={{ marginBottom: "3rem" }}>
+        Find Us
+      </h1>
+      <div className="app__wrapper-content">
+        <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
+        <p
+          className="p__cormorant"
+          style={{ color: "#dcca87", margin: "2rem 0" }}
+        >
+          Working Hours
+        </p>
+        <p className="p__opensans">Monday-Friday: 08:00 am -12:00 am</p>
+        <p className="p__opensans">Saturday-Sunday: 07:00am -11:00 pm</p>
+      </div>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.findus} alt="find us" />
+    </div>
   </div>
 );
 
