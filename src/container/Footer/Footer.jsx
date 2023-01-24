93627d7f31503c48511ead5c2f033dc0b1af257a
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2023-01-24 01:42:36 -0400

    feat: Implement footer component and styles

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
+import { FooterOverlay, Newsletter } from "../../components";
 
-import './Footer.css';
+import "./Footer.css";
 
 const Footer = () => (
-  <div>
-    Footer
-  </div>
+  <footer className="app__footer section__padding">
+    <FooterOverlay />
+    <Newsletter />
+  </footer>
 );
 
 export default Footer;
