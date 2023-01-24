99c8e0b4b88b879443f63edb0c8d6913ff9e1717
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2023-01-23 20:17:52 -0400

    feat: Implement media queries

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -8,10 +8,15 @@ const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
   <div className="app__laurels_awards-card">
     <img src={imgUrl} alt="award" />
     <div className="app__laurels_award-card_content">
-      <p className="p__cormorant" style={{ color: "#ddca87" }}>
+      <p
+        className="p__cormorant"
+        style={{ color: "#ddca87", fontSize: "20px" }}
+      >
         {title}
       </p>
-      <p className="p__cormorant">{subtitle}</p>
+      <p className="p__cormorant" style={{ fontSize: "17px" }}>
+        {subtitle}
+      </p>
     </div>
   </div>
 );
@@ -20,7 +25,7 @@ const Laurels = () => (
   <div className="app__bg app__wrapper section__padding" id="awards">
     <div className="app__wrapper_info">
       <SubHeading title="Awards & Recognition" />
-      <h1 className="headtext_cormorant">Our Laurels</h1>
+      <h1 className="headtext__cormorant">Our Laurels</h1>
       <div className="app__laurels_awards">
         {data.awards.map((award) => (
           <AwardCard key={award.title} award={award} />
